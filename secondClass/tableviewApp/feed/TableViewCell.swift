d6e7d3a27cb62113eb643813a210e07d6c5e1b9b
Sharleen Price <sharleenpprice@gmail.com>
2018-07-29 12:57:58 -0400

    iOS Homework

---
+++ secondClass/tableviewApp/feed/TableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TableViewCell.swift
+//  4thclass
+//
+//  Created by Sharleen Price on 2/19/18.
+//  Copyright © 2018 Sharleen Price. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if (selected) {
+            print("i am selected")
+        }else{
+            print("not selected")
+        }
+        
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
